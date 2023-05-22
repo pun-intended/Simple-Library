4484621c6d769ab031c5ee264c17acb225170de4
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-05-23 07:42:11 +0900

    Format section for story submission

--- hack-or-snooze-ajax-api/js/main.js
+++ hack-or-snooze-ajax-api/js/main.js
@@ -9,6 +9,7 @@ const $allStoriesList = $("#all-stories-list");
 
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
+const $storyForm = $("#story-form");
 
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
