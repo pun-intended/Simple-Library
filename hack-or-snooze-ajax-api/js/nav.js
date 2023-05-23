d215766db0d149b936c259bc1e2bec77eb4efe7f
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-05-23 20:54:58 +0900

    Add navSubmitClick function
    Add function to handle clicks on nav bar for submit

--- hack-or-snooze-ajax-api/js/nav.js
+++ hack-or-snooze-ajax-api/js/nav.js
@@ -34,3 +34,12 @@ function updateNavOnLogin() {
   $navLogOut.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+function navSubmitClick(){
+  console.debug("navSubmitClick");
+  hidePageComponents();
+  $submissionForm.show();
+  $loginForm.hide();
+  $signupForm.hide();
+}
+$navSubmit.on("click", navSubmitClick)
