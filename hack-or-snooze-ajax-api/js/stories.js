d340f1ce6e6ac5be0d59bd3b409e7ae04e223538
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-05-24 08:19:23 +0900

    Add favorite toggle function
    Add a method to add or remove a story from a user's favorites.

--- hack-or-snooze-ajax-api/js/stories.js
+++ hack-or-snooze-ajax-api/js/stories.js
@@ -35,6 +35,47 @@ function generateStoryMarkup(story) {
     `);
 }
 
+/*
+Add or remove a story from a user's favorites given the story ID
+*/
+async function toggleFavorite(id) {
+  const token = currentUser.loginToken;
+  const username = currentUser.username;
+  
+  let action = "POST"
+
+  // If story already exists in list, change method to delete
+  for (let item of currentUser.favorites){
+    if (item.storyId === id){
+      action = "DELETE"
+    }
+  }
+
+  let response = await axios({
+    url: `${BASE_URL}/users/${username}/favorites/${id}`,
+    method: action,
+    params: {
+      "token": token
+    }
+  })
+
+  // update favorites of user
+  currentUser.favorites = response.data.user.favorites
+}
+
+
+function submitStory() {
+  const story = {
+    "title": $("#submission-title").val(),
+    "author": $("#submission-author").val(),
+    "url": $("#submission-url").val()
+  }
+  storyList.addStory(currentUser, story);
+  
+}
+
+$submissionForm.on("submit", submitStory)
+
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
 function putStoriesOnPage() {
