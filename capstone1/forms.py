f974cbb465af58720dd6d11e4f7b9a04130441dd
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-09-02 21:01:32 +0900

    Create forms

---
+++ capstone1/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, IntegerField
+from wtforms.validators import InputRequired, Length, NumberRange
+
+class SignupForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(message="Must provide a username"), 
+                                                   Length(max=20, message="Limit - 20 characters")], )
+    password = PasswordField("Password", validators=[InputRequired(message="Must provide a password")])
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(message="Must input a username")])
+    password = PasswordField("Password", validators=[InputRequired(message="Must input a password")])
+
+class PokemonForm(FlaskForm):
+    name = StringField("PokemonName", validators=[InputRequired(message="Must provide a username")])
+    atk = IntegerField("attack", validators=[NumberRange(min=0, max=15, message="IV must be between 0 and 15")])
+    dfn = IntegerField("defense", validators=[NumberRange(min=0, max=15, message="IV must be between 0 and 15")])
+    hp = IntegerField("hp", validators=[NumberRange(min=0, max=15, message="IV must be between 0 and 15")])
