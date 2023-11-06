dde57e07cb441c4d407e6a84c98e9e35ef6a4020
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-11-06 19:45:27 +0900

    Update seed.py

--- capstone1/seed.py
+++ capstone1/seed.py
@@ -1,16 +1,29 @@
-from models import User, db
+from models import User, UserMon,  db
 
 def seed_users():
     print("seeding Users")
+    
     db.drop_all()
+
+    print("dropped db")
     db.create_all()
 
-    u1 = User.register("test1", "testpass")
-    u2 = User.register("test2", "testpass")
+    print("creates db")
+    
 
-    allUsers = [u1, u2]
+    u1 = User(username="test1", password="testpass")
 
-    db.session.add_all(allUsers)
+    print("Users created")
+    # u2 = User.register("test2", "testpass")
+
+    # allUsers = [u1, u2]
+
+    # db.session.add_all(allUsers)
+    db.session.add(u1)
+
+    print("added Users to session")
     db.session.commit()
+    
+    print("commit complete")
     user = User.query.filter_by(username="test1").first()
-    print(user.username)
+    print(user)
