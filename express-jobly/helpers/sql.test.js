23a3e1e83318e290d65904cb39dde35eaa60eb16
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-11-25 13:01:30 +0900

    Download Base Files

---
+++ express-jobly/helpers/sql.test.js
@@ -0,0 +1,21 @@
+const { sqlForPartialUpdate } = require("./sql")
+
+describe("sqlForPartialUpdate", function() {
+    test("Returns object containing correct string of keys", function() {
+        const updateData = {}
+        const result = 
+    })
+
+    test("Returns object containing all correct values", function() {
+        const updateData = {}
+    })
+    
+    test("Converts column names given jsToSql argument", function() {
+
+    })
+    test("Rejects empty requests", function() {
+        const updateData = {}
+
+    })
+
+} )
