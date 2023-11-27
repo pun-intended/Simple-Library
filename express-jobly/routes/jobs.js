54f3fc962c440ba02fd3f4c224256e7c0dcd9fb6
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-11-27 21:17:10 +0900

    Create post route and tests

--- express-jobly/routes/jobs.js
+++ express-jobly/routes/jobs.js
@@ -1,6 +1,47 @@
-// TODO Create routes
-    // post - admin
-    // get -all - login
-    // get -title login
-    // patch - admin
-    // delete - admin
+"use strict";
+
+/** Routes for jobs. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
+
+const router = new express.Router();
+
+// TODO Complete routes
+router.post('/', ensureAdmin, async function(req, res, next) {
+    try{
+        const validator = jsonschema.validate(req.body, jobNewSchema)
+        console.log(validator.valid)
+        if (!validator.valid){
+            const errs = validator.errors.map(e => e.stack)
+            throw new BadRequestError(errs)
+        }
+
+        const job = await Job.create(req.body)
+        console.log("-----Job created")
+        return res.status(201).json({job})
+    }catch (err) {
+        return next(err)
+    }
+})
+
+router.get('/',ensureLoggedIn, async function(req, res, next) {
+
+})
+
+router.patch('/:id',ensureAdmin, async function(req, res, next) {
+
+})
+
+router.delete('/:id', ensureAdmin, async function(req, res, next) {
+
+})
+
+module.exports = router
--- express-jobly/routes/jobs.test.js
+++ express-jobly/routes/jobs.test.js
@@ -1,24 +1,116 @@
-// TODO Create tests for routes
+"use strict";
+
+const request = require("supertest");
+
+const db = require("../db");
+const app = require("../app");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  u1Token,
+  adminToken,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
     // post - admin
-        // works for admin
-        // fails for user
-        // fails for anon
-        // fails for invalid data
+describe("/POST jobs", function () {
+        const newJob = {
+            title: "new job",
+            salary: 10000,
+            equity: 0.2,
+            company_handle: "c1"
+        }
+    test("works for admin", async function() {
+        const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+        .set("authorization", `Bearer ${adminToken}`);
+
+        expect(resp.statusCode).toBe(201)
+        expect(resp.body).toMatchObject(
+            {job:{
+                id: expect.any(Number),
+                title: "new job",
+                salary: 10000,
+                equity: '0.2',
+                company_handle: "c1"
+            }})
+    })
+    test("fails for user", async function() {
+        const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+        .set("authorization", `Bearer ${u1Token}`);
+
+        expect(resp.statusCode).toBe(401)
+    })
+    test("fails for user", async function() {
+        const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+
+        expect(resp.statusCode).toBe(401)
+    })
+    test("fails for invalid data", async function() {
+        const newJob = {
+            title: "new job",
+            salary: "Wrong",
+            equity: 0.2,
+            company_handle: "c1"
+        }
+
+        const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+        .set("authorization", `Bearer ${adminToken}`);
+
+        expect(resp.statusCode).toBe(400)
+    })
+})
     // get - all - login
+describe("/GET jobs", function () {
         // works for User
+    test("works for admin", async function() {})
         // fails for anon
+    test("fails for user", async function() {})
+})
     // get - title login
+describe("/GET jobs", function () {
         // works for user
+    test("works for admin", async function() {})
         // fails for anon
+    test("fails for user", async function() {})
         // returns 404
+    test("returns 404", async function() {})
+})
     // patch - admin
+describe("/PATCH jobs", function () {
         // works for admin
+    test("works for admin", async function() {})
         // fails for user
+    test("fails for user", async function() {})
         // fails for anon
+    test("fails for user", async function() {})
         // fails for invalid data
+    test("fails for invalid data", async function() {})
         // returns 404
+    test("returns 404", async function() {})
+})
     // delete - admin
+describe("/DELETE jobs", function () {
         // works for admin
+    test("works for admin", async function() {})
         // fails for user
+    test("fails for user", async function() {})
         // fails for anon
+    test("fails for user", async function() {})
         // returns 404
+    test("returns 404", async function() {})
+})
