deaf43f8c8233910d84d980ae2b59143b224b403
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-11-28 21:02:14 +0900

    Complete Routes tests

--- express-jobly/routes/jobs.test.js
+++ express-jobly/routes/jobs.test.js
@@ -2,6 +2,7 @@
 
 const request = require("supertest");
 
+const Job = require("../models/job")
 const db = require("../db");
 const app = require("../app");
 
@@ -13,6 +14,7 @@ const {
   u1Token,
   adminToken,
 } = require("./_testCommon");
+const { NotFoundError } = require("../expressError");
 
 beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
@@ -74,43 +76,204 @@ describe("/POST jobs", function () {
         expect(resp.statusCode).toBe(400)
     })
 })
-    // get - all - login
+
 describe("/GET jobs", function () {
-        // works for User
-    test("works for admin", async function() {})
-        // fails for anon
-    test("fails for user", async function() {})
+    test("works for user", async function() {
+        const resp = await request(app)
+        .get("/jobs")
+        .set("authorization", `Bearer ${u1Token}`);
+
+        expect(resp.statusCode).toBe(200)
+        expect(resp.body).toMatchObject(
+            {jobs: [
+                {
+                id: expect.any(Number),    
+                title:"test1",
+                salary: 1000,  
+                equity: "0.4",
+                company_handle:"c1",
+                },
+                {
+                id: expect.any(Number),
+                title:"test2",
+                salary: 2000,  
+                equity: "0.01",
+                company_handle:"c2",
+                },
+                {
+                id: expect.any(Number),
+                title:"test3",
+                salary: 3000,  
+                equity: "0.03",
+                company_handle:"c3",
+                  },
+            ]}
+        )
+    })
+        
+    test("fails for anon", async function() {
+        const resp = await request(app)
+        .get("/jobs");
+
+        expect(resp.statusCode).toBe(401)
+    })
 })
-    // get - title login
-describe("/GET jobs", function () {
-        // works for user
-    test("works for admin", async function() {})
-        // fails for anon
-    test("fails for user", async function() {})
-        // returns 404
-    test("returns 404", async function() {})
-})
-    // patch - admin
+
 describe("/PATCH jobs", function () {
-        // works for admin
-    test("works for admin", async function() {})
-        // fails for user
-    test("fails for user", async function() {})
-        // fails for anon
-    test("fails for user", async function() {})
-        // fails for invalid data
-    test("fails for invalid data", async function() {})
-        // returns 404
-    test("returns 404", async function() {})
+
+    test("works for admin", async function() {
+        const jobs = await db.query(`
+        SELECT id, company_handle 
+        FROM jobs`)
+
+        const updateData = {
+            title: "new",
+            salary: 100000,
+            equity: 0.1
+        }
+
+        const updateId = jobs.rows[0].id
+
+        const resp = await request(app)
+        .patch(`/jobs/${updateId}`)
+        .send(updateData)
+        .set("authorization", `Bearer ${adminToken}`);
+
+        expect(resp.statusCode).toBe(200)
+
+        const updatedJob = await Job.get(updateId)
+        expect(updatedJob).toEqual({
+            id: updateId,
+            title: "new",
+            salary: 100000,
+            equity: '0.1',
+            company_handle: jobs.rows[0].company_handle
+        })
+    })
+
+    test("fails for user", async function() {
+        const jobs = await db.query(`
+        SELECT id, company_handle FROM jobs`)
+
+        const updateData = {
+            title: "new",
+            salary: 100000,
+            equity: 0.0
+        }
+
+        const updateId = jobs.rows[0].id
+        
+        const resp = await request(app)
+        .patch(`/jobs/${updateId}`)
+        .send(updateData)
+        .set("authorization", `Bearer ${u1Token}`)
+
+        expect(resp.statusCode).toBe(401)
+    })
+
+    test("fails for anon", async function() {
+        const jobs = await db.query(`
+        SELECT id, company_handle FROM jobs`)
+
+        const updateData = {
+            title: "new",
+            salary: 100000,
+            equity: 0.0
+        }
+
+        const updateId = jobs.rows[0].id
+        
+        const resp = await request(app)
+        .patch(`/jobs/${updateId}`)
+        .send(updateData)
+        .set("authorization", `Bearer ${u1Token}`)
+
+        expect(resp.statusCode).toBe(401)
+    })
+
+    test("fails for invalid data", async function() {
+        const jobs = await db.query(`
+        SELECT id, company_handle FROM jobs`)
+
+        const updateData = {
+            title: 100,
+            salary: "nope",
+            equity: 0.0
+        }
+
+        const updateId = jobs.rows[0].id
+        
+        const resp = await request(app)
+        .patch(`/jobs/${updateId}`)
+        .send(updateData)
+        .set("authorization", `Bearer ${adminToken}`)
+
+        expect(resp.statusCode).toBe(400)
+    })
+
+    test("returns 404", async function() {
+        const updateData = {
+            title: "new",
+            salary: 100000,
+            equity: 0.0
+        }
+        
+        const resp = await request(app)
+        .patch(`/jobs/0`)
+        .send(updateData)
+        .set("authorization", `Bearer ${adminToken}`)
+
+        expect(resp.statusCode).toBe(404)
+
+    })
 })
-    // delete - admin
+
 describe("/DELETE jobs", function () {
-        // works for admin
-    test("works for admin", async function() {})
-        // fails for user
-    test("fails for user", async function() {})
-        // fails for anon
-    test("fails for user", async function() {})
-        // returns 404
-    test("returns 404", async function() {})
+
+    test("works for admin", async function() {
+        const jobs = await db.query(`
+        SELECT id FROM jobs
+        `)
+        const deleteId = jobs.rows[0].id
+
+        const resp = await request(app)
+        .delete(`/jobs/${deleteId}`)
+        .set("authorization", `Bearer ${adminToken}`)
+
+        expect(resp.statusCode).toBe(200)
+    })
+
+    test("fails for user", async function() {
+        const jobs =await db.query(`
+        SELECT id FROM jobs
+        `)
+        const deleteId = jobs.rows[0].id
+
+        const resp = await request(app)
+        .delete(`/jobs/${deleteId}`)
+        .set('authorization', `Bearer ${u1Token}`)
+
+        expect(resp.statusCode).toBe(401)
+    })
+
+    test("fails for anon", async function() {
+        const jobs = await db.query(`
+        SELECT id FROM jobs
+        `)
+        const deleteId = jobs.rows[0].id
+
+        const resp = await request(app)
+        .delete(`/jobs/${deleteId}`)
+
+        expect(resp.statusCode).toBe(401)
+    })
+
+    test("returns 404", async function() {
+
+        const resp = await request(app)
+        .delete(`/jobs/0`)
+        .set("authorization", `Bearer ${adminToken}`)
+
+        expect(resp.statusCode).toBe(404)
+    })
 })
