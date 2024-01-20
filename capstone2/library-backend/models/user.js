584c3efb204148e0666c45fb818482a6f1995e89
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-20 14:44:50 +0900

    Complete user methods

--- capstone2/library-backend/models/user.js
+++ capstone2/library-backend/models/user.js
@@ -1,6 +1,7 @@
 "use strict"
 
-const db = require('../db')
+const db = require('../db');
+const { NotFoundError, UnauthorizedError } = require('../expressError');
 // Error handlers
 // sql for partial update method
 
@@ -10,11 +11,62 @@ class User {
     /**
      * Create a new user with the given data
      * 
-     * {data} => {token}
+     * {id, first_name, last_name, password, isAdmin} => 
+     *      {id, first_name, last_name, isAdmin}
      * 
      * Throws NotFoundError if id not found
      */
-    static async create(data){}
+    static async create({id, first_name, last_name, password, isAdmin}){
+        const duplicateCheck = await db.query(
+            `SELECT id
+            FROM users
+            WHERE id = $1`,
+            [id]
+        );
+
+        if (duplicateCheck.rows[0]) {
+            throw new BadRequestError(`Duplicate user ID: ${id}`);
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+        
+        const res = db.query(`
+        INSERT INTO users 
+            (id,
+            first_name,
+            last_name,
+            password,
+            is_admin)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, first_name, last_name, is_admin`,
+        [id, first_name, last_name, hashedPassword, isAdmin]);
+
+        const user = res.rows[0];
+
+        return user;
+    }
+
+    /**
+     * Get a given user
+     * 
+     * {id} => {id, first_name, last_name, isAdmin}
+     * 
+     * Throws NotFoundError if user id not found
+     */
+
+    static async get(id){
+        const res = await db.query(`
+        SELECT id, first_name, last_name, isAdmin
+        FROM users
+        WHERE id = $1`,
+        [id]);
+
+        const user = res.rows[0];
+
+        if (!user) throw new NotFoundError(`No user found with ID ${id}`);
+
+        return user;
+    }
 
     /**
      * Delete a given user
@@ -23,21 +75,87 @@ class User {
      * 
      * Throws NotFoundError if id not found
      */
-    static async remove(id){}
+    static async remove(id){
+        const res = await db.query(`
+        DELETE FROM users
+        WHERE id = $1
+        RETURNING id`,
+        [id]);
+
+        const user = res.rows[0];
+
+        if (!user) throw new NotFoundError(`No user found with ID ${id}`);
+
+        return {deleted: user};
+    }
 
     /**
      * Update user data
      * 
      * {data} => {updated: id, first_name, last_name}
      * 
+     * Data can include {firstName, lastName, password, isAdmin}
+     * 
      * Throws NotFoundError if id not found
      */
-    static async update(data){}
+    // TODO - Check all instances of first_name etc for naming conventions
+    static async update(id, data) {
+        if (data.password) {
+          data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+        }
+    
+        const { setCols, values } = sqlForPartialUpdate(
+            data,
+            {
+              firstName: "first_name",
+              lastName: "last_name",
+              isAdmin: "is_admin",
+            });
+        const userIdVarIdx = "$" + (values.length + 1);
+    
+        const querySql = `UPDATE users 
+                          SET ${setCols} 
+                          WHERE id = ${userIdVarIdx} 
+                          RETURNING id,
+                                    first_name AS "firstName",
+                                    last_name AS "lastName",
+                                    email,
+                                    is_admin AS "isAdmin"`;
+        const result = await db.query(querySql, [...values, id]);
+        const user = result.rows[0];
+    
+        if (!user) throw new NotFoundError(`No user: ${id}`);
+    
+        delete user.password;
+        return user;
+      }
 
     /**
      * Authenticate username and Password
      * 
-     * {user_id, password} => {user_id, first_name, last_name, is_admin}
+     * {user_id, password} => {id, first_name, last_name, is_admin}
      */
-    static async authenticate(user_id, password){}
+    static async authenticate(id, password){
+        const result = await db.query(`
+        SELECT  id, 
+                password, 
+                first_name AS "firstName",
+                last_name AS "lastName",
+                is_admin AS "isAdmin"
+        FROM users
+        WHERE id = $1`,
+        [id]);
+
+        const user = res.rows[0];
+
+        if(user){
+            const isValid = await bcrypt.compare(password, user.password);
+            if(isValid){
+                delete user.password;
+                return user;
+            }
+        }
+
+        throw new UnauthorizedError("Invalid id/password");
+    }
 }
