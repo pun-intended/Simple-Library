db44565a812414b1d08b02c122061bd3d3c67eee
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-20 10:56:14 +0900

    Add basic methods
    Add necessary methods, and definitions

--- capstone2/library-backend/models/student.js
+++ capstone2/library-backend/models/student.js
@@ -8,21 +8,29 @@ class Student {
 
     /**
      * Create new student record
+     * 
+     * {data} = {created: {id, first_name, last_name, level}}
      */
-    static async create(){}
+    static async create(data){}
     
     /**
      * Return details on Student
+     * 
+     * {id} => {id, first_name, last_name, level}
      * @param {*} studentId 
      */
     static async getStudent(studentId){}
 
     /**
      * Return an array with book IDs for books student has read.
+     * 
+     * {id} => [{id, isbn, title, stage, condition}, ...] 
      * @param {} studentId 
      */
     static async getReadBooks(studentId){}
 
+
+    //TODO - STRETCH
     /**
      * Set students class
      */
--- capstone2/library-backend/models/user.js
+++ capstone2/library-backend/models/user.js
@@ -7,11 +7,37 @@ const db = require('../db')
 // ----- After completion
 class User {
 
-    static async create(){}
+    /**
+     * Create a new user with the given data
+     * 
+     * {data} => {token}
+     * 
+     * Throws NotFoundError if id not found
+     */
+    static async create(data){}
 
-    static async remove(){}
+    /**
+     * Delete a given user
+     * 
+     * {id} => {deleted: id}
+     * 
+     * Throws NotFoundError if id not found
+     */
+    static async remove(id){}
 
-    static async update(){}
+    /**
+     * Update user data
+     * 
+     * {data} => {updated: id, first_name, last_name}
+     * 
+     * Throws NotFoundError if id not found
+     */
+    static async update(data){}
 
-    static async authenticate(){}
+    /**
+     * Authenticate username and Password
+     * 
+     * {user_id, password} => {user_id, first_name, last_name, is_admin}
+     */
+    static async authenticate(user_id, password){}
 }
