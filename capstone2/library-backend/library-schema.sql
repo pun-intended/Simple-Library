6e8c2ce488ed6c84bd8c0f935ce23d01a35ef1f7
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-20 12:22:32 +0900

    Comment out stretch goals, change ISBN data type

--- capstone2/library-backend/library-schema.sql
+++ capstone2/library-backend/library-schema.sql
@@ -1,46 +1,48 @@
--- table for users
--- TODO - Change id to serial
--- STRETCH class_id, school
+-- CREATE TABLE schools (
+--     id INTEGER PRIMARY KEY,
+--     name VARCHAR(25) NOT NULL,
+--     location TEXT NOT NULL
+-- );
+
+-- CREATE TABLE classes (
+--     id INTEGER PRIMARY KEY,
+--     name VARCHAR(20) NOT NULL,
+--     school_id INTEGER NOT NULL,
+-- );
+
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY ,
+    id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    class_id INTEGER
-        REFERENCES classes ON DELETE SET NULL,
-    school_id INTEGER
-        REFERENCES schools ON DELETE SET NULL,
+    -- class_id INTEGER
+    --     REFERENCES classes ON DELETE SET NULL,
+    -- school_id INTEGER
+    --     REFERENCES schools ON DELETE SET NULL,
     password TEXT NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
-)
+);
 
--- table for students (add age?)
--- TODO - Change id to serial
 CREATE TABLE students (
     id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    school_id INTEGER
-        REFERENCES schools ON DELETE SET NULL,
-    class_id INTEGER
-        REFERENCES classes ON DELETE SET NULL,
+    -- school_id INTEGER
+    --     REFERENCES schools ON DELETE SET NULL,
+    -- class_id INTEGER
+    --     REFERENCES classes ON DELETE SET NULL,
     level TEXT NOT NULL
-)
+);
 
--- table for books
--- STRETCH set, school_id values
--- TODO - Change id to serial
 CREATE TABLE books (
     id INTEGER PRIMARY KEY,
-    isbn INTEGER NOT NULL,
+    isbn TEXT NOT NULL,
     title TEXT NOT NULL,
     stage INTEGER NOT NULL,
     set INTEGER,
-    school_id INTEGER,
+    -- school_id INTEGER,
     condition VARCHAR(10) NOT NULL
-)
+);
 
--- table for borrow record
--- TODO - Change id to serial
 CREATE TABLE borrow_record (
     id SERIAL PRIMARY KEY, 
     student_id INTEGER NOT NULL
@@ -49,20 +51,4 @@ CREATE TABLE borrow_record (
         REFERENCES books ON DELETE CASCADE,
     borrow_date DATE NOT NULL,
     return_date DATE
-)
-
--- STRETCH table for schools
-CREATE TABLE schools (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(25) NOT NULL,
-    location TEXT NOT NULL
-)
-
--- STRETCH table for classes
-CREATE TABLE classes (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    school_id INTEGER NOT NULL,
-)
-
--- 
+);
