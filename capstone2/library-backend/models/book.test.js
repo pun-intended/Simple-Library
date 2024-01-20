8e3eaf62da41699eec90415e7b1806005bcb6c1f
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-20 17:47:10 +0900

    Create test files

---
+++ capstone2/library-backend/models/book.test.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const db = require('../db.js');
+const {BadRequestError, NotFoundError } = require('../expressError.js');
+const {
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll
+ } = require('./_testCommon.js');
+
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
