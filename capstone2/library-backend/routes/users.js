0259b68afe04d7e5cf11c87e631cb1d14596f097
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-20 18:32:33 +0900

    Complete user routes

--- capstone2/library-backend/routes/users.js
+++ capstone2/library-backend/routes/users.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** Routes for companies. */
+/** Routes for users. */
 
 // const jsonschema = require("jsonschema");
 const express = require("express");
@@ -8,7 +8,65 @@ const express = require("express");
 const { BadRequestError } = require("../expressError");
 // const { ensureAdmin } = require("../middleware/auth");
 const User = require("../models/user");
+const { createToken } = require("../helpers/tokens");
 
 // const schema = require("../schemas/schema.json");
 
 const router = new express.Router();
+
+/** Add user */
+router.post("/", async function (req, res, next) {
+    // Add Validation
+    try{
+        const user = await User.create(req.body)
+        const token = createToken(user)
+        return res.status(201).json({ user, token })
+    }catch(e){
+        return next(e)
+    }
+})
+
+/** Get all users */
+router.get("/", async function (req, res, next) {
+    try{
+        const users = await User.getAll();
+        return res.json({users})
+    } catch (e) {
+        return next(e)
+    }
+});
+
+/** Get user by ID */
+router.get("/:id", async function (req, res, next) {
+    try{
+        const user = await User.get(req.params.id);
+        return res.json({user})
+    } catch (e) {
+        return next(e)
+    }
+})
+
+/** Update user */
+router.patch("/:id", async function (req, res, next) {
+    try{
+        // Add validation
+        const user = await User.update(req.params.id, req.body);
+        return res.json({user})
+    } catch (e) {
+        return next(e)
+    }
+})
+
+/** Delete user */
+router.delete("/:id", async function (req, res, next) {
+    try{
+        await User.delete(req.params.id);
+        return res.json({deleted: req.params.id})
+    } catch (e) {
+        return next(e)
+    }
+})
+
+
+
+module.exports = router;
