f0cba14f547d771af191ad7ec16f010c69f3e491
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-20 17:47:38 +0900

    Add common test cases

---
+++ capstone2/library-backend/models/_testCommon.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+
+const db = require("../db.js");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+
+async function commonBeforeEach() {
+    await db.query("BEGIN");
+}
+
+    async function commonAfterEach() {
+await db.query("ROLLBACK");
+}
+
+async function commonAfterAll() {
+    await db.end();
+}
+
+module.exports = {
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll,
+};
