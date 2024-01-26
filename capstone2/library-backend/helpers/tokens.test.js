f1691eeb9a39679e58829a8908532f24eb08afec
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-27 08:42:47 +0900

    Update tokens.test.js

--- capstone2/library-backend/helpers/tokens.test.js
+++ capstone2/library-backend/helpers/tokens.test.js
@@ -4,33 +4,33 @@ const { SECRET_KEY } = require("../config");
 
 describe("createToken", function () {
   test("works: not admin", function () {
-    const token = createToken({ username: "test", is_admin: false });
+    const token = createToken({ id: 1, is_admin: false });
     const payload = jwt.verify(token, SECRET_KEY);
     expect(payload).toEqual({
       iat: expect.any(Number),
-      username: "test",
-      isAdmin: false,
+      id: 1,
+      is_admin: false,
     });
   });
 
   test("works: admin", function () {
-    const token = createToken({ username: "test", isAdmin: true });
+    const token = createToken({ id: 1, is_admin: true });
     const payload = jwt.verify(token, SECRET_KEY);
     expect(payload).toEqual({
       iat: expect.any(Number),
-      username: "test",
-      isAdmin: true,
+      id: 1,
+      is_admin: true,
     });
   });
 
   test("works: default no admin", function () {
     // given the security risk if this didn't work, checking this specifically
-    const token = createToken({ username: "test" });
+    const token = createToken({ id: 1, });
     const payload = jwt.verify(token, SECRET_KEY);
     expect(payload).toEqual({
       iat: expect.any(Number),
-      username: "test",
-      isAdmin: false,
+      id: 1,
+      is_admin: false,
     });
   });
 });
