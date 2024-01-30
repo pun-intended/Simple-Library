e5856fa73e3063224c6db0e0116df9c0d596d217
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 18:34:37 +0900

    Add check-in/check-out modals

--- capstone2/library-frontend/src/students/StudentCard.js
+++ capstone2/library-frontend/src/students/StudentCard.js
@@ -1,26 +1,39 @@
 // Name, class, Book cover if they have checked out
 // book - onClick ->BookDetails
-import React from "react"
-import {Card, CardBody, CardTitle, CardText, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import React, {useState} from "react"
+import { Row, Col, Card, CardBody, CardTitle, CardText, Button } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBook, faHandHolding } from '@fortawesome/free-solid-svg-icons'
+import { faBookOpen, faHandHolding } from '@fortawesome/free-solid-svg-icons'
+import CheckOutStudent from "./CheckOutStudent"
+import CheckInModal from "../books/CheckInModal"
 
 
-const StudentCard = ({student}) => {
-    async function click(){
-        console.log("click")
-    }
+const StudentCard = ({student, setUpdate}) => {
+    
+    const [outModal, setOutModal] = useState(false);
+    const [inModal, setInModal] = useState(false);
+    const toggleOut = () => { setOutModal(!outModal)};
+    const toggleIn = () => { setInModal(!inModal)};
+    
     return(
         <Card className="StudentCard">
+            <Row>
+            <Col >
             <CardTitle >{student.first_name} {student.last_name}</CardTitle>
-
-            {student.book_id &&
-                <FontAwesomeIcon icon={faBook} size="lg" className="btn-outline-primary" onClick={click} />
+            </Col>
+            <Col>
+            {student.book_id && 
+                <FontAwesomeIcon icon={faBookOpen} size="lg" className="btn btn-primary" onClick={toggleIn} />
             }
 
             {!student.book_id &&
-                <FontAwesomeIcon icon={faHandHolding} onClick={click}/>}
+                <FontAwesomeIcon icon={faHandHolding} className="btn btn-primary" onClick={toggleOut}/>}
+            {outModal && <CheckOutStudent modal={outModal} toggle={toggleOut} student={student} setUpdate={setUpdate}/>}
+            {inModal && <CheckInModal modal={inModal} toggle={toggleIn} book_id={student.book_id} setUpdate={setUpdate}/>}
+            </Col>
+            </Row>
         </Card>
+        
     )
 }
 
