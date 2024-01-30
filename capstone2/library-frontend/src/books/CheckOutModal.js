4a587119429e9b2b13634718c1ed0610e4cd3a8b
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 13:31:46 +0900

    Add form functionality to Modal

--- capstone2/library-frontend/src/books/CheckOutModal.js
+++ capstone2/library-frontend/src/books/CheckOutModal.js
@@ -1,8 +1,55 @@
-import React, { useState } from "react"
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Container, Col, Row } from 'reactstrap'
+import React, { useContext, useState } from "react"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Container, Col, Row, Input, FormGroup } from 'reactstrap'
+import {DatePicker} from "reactstrap-date-picker"
 import "./CheckOutModal.css"
+import StudentContext from "../StudentContext";
+import LibraryApi from "../api";
 
-const CheckOutModal = ({modal, toggle, book}) => {
+
+const CheckOutModal = ({modal, toggle, book , setUpdate}) => {
+
+    const students = useContext(StudentContext)
+    const [values, setValues] = useState([])
+
+    const INITIAL_STATE = {
+        'student_id': "",
+        'date': new Date().toISOString().slice(0, 10),
+        'book_id': book.id.toString()
+    }
+
+    const[formData, setFormData] = useState(INITIAL_STATE)
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        try{
+            console.log(formData)
+            LibraryApi.checkOut(formData)
+            setFormData(INITIAL_STATE)
+            setUpdate(true);
+            toggle()
+        } catch(e){
+            console.log(e)
+        }
+    }
+
+    const handleDateChange = (v, f) => {
+        console.log(`v - ${v}`)
+        console.log(`f - ${f}`)
+        setFormData(fData => ({
+            ...fData,
+            'date': f
+        }));
+        console.log(formData)
+    }
+
+    const handleChange = evt => {
+        const { name, value } = evt.target
+        setFormData(fData => ({
+            ...fData,
+            [name]: value
+        }));
+        console.log(formData)
+    }
 
     return(
         <Modal isOpen={modal} toggle={toggle} backdrop="true">
@@ -17,12 +64,36 @@ const CheckOutModal = ({modal, toggle, book}) => {
                                 alt={`${book.title}`}/>
                         </Col>
                         <Col>
+                        <FormGroup id="checkoutForm">
+                            <Input  type="select" 
+                                    name="student_id" 
+                                    id="selectStudent" 
+                                    value={formData.student}
+                                    onChange={handleChange}>
+                            <option>--Select Student</option>
+                                {students.map(st => {
+                                    return (
+                                    <option value={parseInt(st.id)}>
+                                        {st.first_name} {st.last_name}
+                                    </option>
+                                    )
+                                })}
+                            </Input>
+                            <DatePicker name="date" 
+                                        id="datePicker"
+                                        defaultValue={formData.date}
+                                        dateFormat="YYYY-MM-DD"
+                                        placeholder="YYYY-MM-DD"
+                                        onChange={handleDateChange}
+                                        disabled={!formData.student_id}
+                                        />
+                        </FormGroup>
                         </Col>
                     </Row>
                 </Container>
             </ModalBody>
             <ModalFooter>
-                <Button color="primary" onClick={toggle}>Check Out</Button>
+                <Button color="primary" onClick={handleSubmit}>Check Out</Button>
                 <Button color="secondary" onClick={toggle}>Cancel</Button>
             </ModalFooter>
         </Modal>
