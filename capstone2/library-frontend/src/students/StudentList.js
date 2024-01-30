93c6fa64c1ce80ad2d79acbb1e6cef2d070ceac8
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 18:34:54 +0900

    Add update trigger for students

--- capstone2/library-frontend/src/students/StudentList.js
+++ capstone2/library-frontend/src/students/StudentList.js
@@ -9,7 +9,18 @@ import "./StudentCard.css";
 import StudentContext from "../StudentContext";
 
 const StudentList = () => {
-    const students = useContext(StudentContext)
+    const [update, setUpdate] = useState(false)
+    const {students, setStudents} = useContext(StudentContext) 
+    // const [students, setStudents] = studentState
+    
+    useEffect( () => {
+        async function initializeList(){
+            const allStudents = await LibraryApi.getAllStudents()
+            setStudents(allStudents)
+            setUpdate(false)
+        }
+        initializeList()
+    }, [update])
 
     return(
         <div className="StudentList">
@@ -18,8 +29,8 @@ const StudentList = () => {
                 <Row>
             {students.map((st) =>  {
                 return (
-                    <Col sm="2">
-                    <StudentCard student={st} />
+                    <Col xs="6" sm="4" md="3" lg="2">
+                    <StudentCard student={st} setUpdate={setUpdate} key={st.id}/>
                     </Col>
                 )})}
                 </Row>
