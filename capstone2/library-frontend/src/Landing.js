0fe2394a5167fed3cbb9591d215fa7bbbd2bf35d
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 19:06:19 +0900

    Clean up landing page

--- capstone2/library-frontend/src/Landing.js
+++ capstone2/library-frontend/src/Landing.js
@@ -2,20 +2,10 @@ import React, { useEffect, useState } from "react"
 import LibraryApi from "./api"
 
 function Landing () {
-    const [result, setResult] = useState()
-    useEffect( () => {
-        async function getConn(){
-            const res = await LibraryApi.testConnection()
-            setResult(res.result)
-        }
-        getConn()
-    }
-    )
 
     return (
         <div>
             <h2>Welcome to The Library!</h2>
-            <h3>{result}</h3>
         </div>
     )
 }
