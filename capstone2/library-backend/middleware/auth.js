5fdca619550c1767cd9e4fcf0926bb50d214ce95
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 19:05:24 +0900

    Adjust middleware to handle string ids

--- capstone2/library-backend/middleware/auth.js
+++ capstone2/library-backend/middleware/auth.js
@@ -68,7 +68,7 @@ function ensureAdmin(req, res, next) {
 function ensureCorrectUserOrAdmin(req, res, next) {
   try {
     const user = res.locals.user;
-    if (!(user && (user.is_admin || user.id === req.params.id))) {
+    if (!(user && (user.is_admin || user.id == req.params.id))) {
       throw new UnauthorizedError();
     }
     return next();
