9b686fc32199cc8fe797dc96402dd33791f99190
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 13:31:00 +0900

    Create CheckInModal.js

---
+++ capstone2/library-frontend/src/books/CheckInModal.js
@@ -0,0 +1,117 @@
+import React, { useContext, useEffect, useState } from "react"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Container, Col, Row, Input, FormGroup } from 'reactstrap'
+import {DatePicker} from "reactstrap-date-picker"
+import "./CheckOutModal.css"
+import StudentContext from "../StudentContext";
+import LibraryApi from "../api";
+
+
+const CheckOutModal = ({modal, toggle, book, setUpdate}) => {
+
+    let bookDetails;
+
+    
+
+    useEffect(() => {
+        async function getBookDetails() {
+            bookDetails = await LibraryApi.getBook(book.id)
+        }
+        getBookDetails()
+    }, [])
+
+    const INITIAL_STATE = {
+        'date': new Date().toISOString().slice(0, 10),
+        'book_id': book.id.toString(),
+        'condition': "good"
+    }
+
+    const students = useContext(StudentContext)
+    const[formData, setFormData] = useState(INITIAL_STATE)
+    
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        try{
+            console.log(formData)
+            LibraryApi.checkIn(formData)
+            setFormData(INITIAL_STATE)
+            setUpdate(true);
+            toggle()
+        } catch(e){
+            console.log(e)
+        }
+    }
+
+    const handleDateChange = (v, f) => {
+        console.log(`v - ${v}`)
+        console.log(`f - ${f}`)
+        setFormData(fData => ({
+            ...fData,
+            'date': f
+        }));
+        console.log(formData)
+    }
+
+    // For changing condition
+    const handleChange = evt => {
+        const { name, value } = evt.target
+        setFormData(fData => ({
+            ...fData,
+            [name]: value
+        }));
+        console.log(formData)
+    }
+
+    return(
+        <Modal isOpen={modal} toggle={toggle} backdrop="true">
+            <ModalHeader toggle={toggle}>
+                Check-In - {`${book.title}`}
+            </ModalHeader>
+            <ModalBody>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`} 
+                                alt={`${book.title}`}/>
+                        </Col>
+                        <Col>
+                            <h3>Borrowing: </h3>
+                            <FormGroup id="checkoutForm">
+                            <DatePicker name="date" 
+                                        id="datePicker"
+                                        value={formData.date}
+                                        dateFormat="YYYY-MM-DD"
+                                        placeholder="YYYY-MM-DD"
+                                        onChange={handleDateChange}
+                                        />
+                            <Input  type="select"
+                                    name="condition"
+                                    id="condition"
+                                    value={formData.condition}
+                                    onChange={handleChange}
+                                    placeholder="--Condition"
+                            >
+                                <option>
+                                    Poor
+                                </option>
+                                <option>
+                                    Good
+                                </option>
+                                <option>
+                                    Great
+                                </option>
+                            </Input>
+                        </FormGroup>
+                        </Col>
+                    </Row>
+                </Container>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={handleSubmit}>Check In</Button>
+                <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+        </Modal>
+    )
+}
+
+export default CheckOutModal
+
