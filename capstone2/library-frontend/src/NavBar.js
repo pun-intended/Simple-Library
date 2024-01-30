5272b8614e0a92b8daa92325a53fe55bf9ad7250
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-30 19:06:46 +0900

    Hide books/students if not logged in

--- capstone2/library-frontend/src/NavBar.js
+++ capstone2/library-frontend/src/NavBar.js
@@ -13,27 +13,33 @@ const NavBar = () =>{
         <NavLink exact to="/" className="navbar-brand">
           Library
         </NavLink>
-
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink to="/books">Books</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/students">Students</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/login">Login</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/signup">Signup</NavLink>
-          </NavItem>
-        </Nav>
         {currentUser && <NavLink to="/profile" className="navbar-brand">
           {`${currentUser.id} - ${currentUser.first_name}`}
         </NavLink>}
-        {currentUser && <NavLink exact to="/logout" className="navbar-brand">
-          Logout
-        </NavLink>}
+
+        <Nav className="ml-auto" navbar>
+          {currentUser &&
+          <NavItem>
+            <NavLink to="/books">Books</NavLink>
+          </NavItem>}
+          {currentUser &&
+          <NavItem>
+            <NavLink to="/students">Students</NavLink>
+          </NavItem>}
+          {!currentUser &&
+          <NavItem>
+            <NavLink to="/login">Login</NavLink>
+          </NavItem>}
+          {!currentUser &&
+          <NavItem>
+            <NavLink to="/signup">Signup</NavLink>
+          </NavItem>}
+          {currentUser && 
+          <NavItem>
+            <NavLink exact to="/logout"> Logout </NavLink>
+          </NavItem>}
+        </Nav>
+        
       </Navbar>
     </div>
   );
