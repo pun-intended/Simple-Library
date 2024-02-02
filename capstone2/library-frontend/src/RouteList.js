6a9d2566973ae5db0ef1d5a08e180dfbbe490ed5
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-02-02 14:16:35 +0900

    Remove unnecessary elements

--- capstone2/library-frontend/src/RouteList.js
+++ capstone2/library-frontend/src/RouteList.js
@@ -1,10 +1,7 @@
 import React from "react";
-import App from "./App";
-import LibraryApi from "./api";
 import { Route, Routes } from "react-router-dom"
 import LoginForm from "./users/LoginForm"
 import LogOut from "./users/LogOut";
-import CheckInList from "./books/CheckInList"
 import StudentList from "./students/StudentList"
 import BookList from "./books/BookList"
 import Landing from './Landing';
@@ -15,10 +12,8 @@ function RouteList({login, logout}) {
         <Routes>
             <Route exact path="/login" element={<LoginForm login={login}/>} />
             <Route exact path="/logout" element={<LogOut logout={logout}/>} />
-            <Route exact path="/checkin" element={<CheckInList />} />
             <Route exact path="/students" element={<StudentList />} />
             <Route exact path="/books" element={<BookList />} />
-            {/* <Route exact path="/signup" element={<SignUpForm />} /> */}
             <Route exact path="/" element={<Landing />} />
         </Routes>
     )
