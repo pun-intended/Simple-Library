67a722a7981efc80b973aa75fa65fd911a461556
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-03-06 22:57:08 +0900

    Add buttons for testing alerts (commented out)

--- capstone2/library-frontend/src/Landing.js
+++ capstone2/library-frontend/src/Landing.js
@@ -1,11 +1,23 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useContext } from "react"
+import { Button } from "reactstrap"
 import LibraryApi from "./api"
+import AlertContext from "./AlertContext"
 
 function Landing () {
+
+    // const {alerts, setAlerts, addAlert, removeAlert} = useContext(AlertContext)
+    // function handleClick(evt){
+    //     console.log("New Alert")
+    //     const target = evt.target
+    //     addAlert("test alert", target.value)
+    // }
 
     return (
         <div>
             <h2>Welcome to The Library!</h2>
+            {/* <Button color="primary" value="primary" onClick={handleClick}>Add Primary Alert</Button>
+            <Button color="success" value="success" onClick={handleClick}>Add success Alert</Button>
+            <Button color="danger" value="danger" onClick={handleClick}>Add danger Alert</Button> */}
         </div>
     )
 }
