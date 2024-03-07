ab9ce3067497e4efaa61edf8e5128cea690acb50
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-03-07 21:46:45 +0900

    Update cors origin, methods, credentials

--- capstone2/library-backend/app.js
+++ capstone2/library-backend/app.js
@@ -20,7 +20,13 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://lending-library-xjrg.onrender.com",
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
