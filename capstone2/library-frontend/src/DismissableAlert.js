56af780fa6ac50cc39b99fbffb1f45fa73d30bfa
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-03-09 13:58:01 +0900

    Add timeout

--- capstone2/library-frontend/src/DismissableAlert.js
+++ capstone2/library-frontend/src/DismissableAlert.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { Alert } from "reactstrap";
 
 function DismissableAlert(message, color, id, removeAlert) {
+    window.setTimeout(() => {
+      // TODO - cosmetic - Fade this out rather than abrupt change
+      removeAlert(id)
+    }, 5000)
     return(
       <Alert color={color} isOpen={true} id={id} toggle={() => { removeAlert(id) }}>{message}</Alert>
     )
