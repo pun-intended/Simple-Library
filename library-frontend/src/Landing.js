a98a79e14abc55c09fe041b3687cf09c608f7d77
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-06-22 17:55:16 +0900

    Add alert when choosing token

--- library-frontend/src/Landing.js
+++ library-frontend/src/Landing.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState, useContext } from "react"
+import React, { useContext } from "react"
 import { Button } from "reactstrap"
-import LibraryApi from "./api"
 import AlertContext from "./AlertContext"
 
 const Landing = ({setToken}) => {
+    const {addAlert} = useContext(AlertContext)
 
         function handleClick(evt){
-            console.log("token chosen")
             const target = evt.target
-    
+            addAlert(`Token Chosen - ${target.value}`)
             setToken(target.value)
+
         }
     
         return (
@@ -20,12 +20,7 @@ const Landing = ({setToken}) => {
             </div>
         )
     }
-    // Dev
-    // let user = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDEsImlzX2FkbWluIjpmYWxzZSwiaWF0IjoxNTE2MjM5MDIyfQ.o7FLElxzQ2rSoTboMXB-zOVOfIBT4z_qYBMVV_xHvNc`
-    // let admin = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1MTYyMzkwMjJ9.f5hMoreRwO94IBwRYJFFWb9dbxMKIkimi1VluwW_QM8`
-    
-    //Demo
-    // 6695b3afa0b10c858d63789372534a5f
+
     let user = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDEsImlzX2FkbWluIjpmYWxzZSwiaWF0IjoxNTE2MjM5MDIyfQ.LcKc9HcruW68Q3jbcnrEwuwvzu31MDHl2BLJLQdSTU8`
     let admin = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1MTYyMzkwMjJ9.0ShJ-JgKDHVNSuqdUS_DwWm8KO1BQj6OxW7N_nQ7CHQ`
     
