8f8c696bc77018ef7aa922e898f86b4c1250a70d
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-06-22 17:25:22 +0900

    Style BookList book cards
    Add responsiveness to sizes
    Correct font and button sizes to match view width
    Add responsive row widths for book list

--- library-frontend/src/books/BookCard.js
+++ library-frontend/src/books/BookCard.js
@@ -1,6 +1,7 @@
 // Simple card for list
 // Cover (l, set height), title, who has, borrowed date
 // OnClick function pass
+import "./BookCard.css"
 import React, { useState } from "react"
 import {Col, Row, Button } from 'reactstrap'
 import CheckOutModal from "./CheckOutModal.js";
@@ -15,23 +16,20 @@ const BookCard = ({book, setUpdate}) => {
 
     return(
             <Col    
-                className={`bookCard col-6 col-sm-6 col-md-4 col-lg-3`}>
-                <Row className="d-flex ">
+                className={`d-flex bookCard col-6 col-sm-4 col-md-3 col-lg-2`}>
                     <Col xs="6"
                         className="bg-light border d-flex flex-row bookImage rounded-left">
                         <img    src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`} 
-                            className="object-fit-scale mx-auto rounded"
+                            className="object-fit-scale mx-auto rounded justify-content-center"
                             alt={`${book.title}`}
                         />
                     </Col>
                     <Col xs="6"
-                        className="bg-light border d-flex flex-column bookTitle rounded-right justify-content-around">
+                        className="bg-light border d-flex flex-column bookTitle rounded-right justify-content-between">
                         <h4>{book.title}</h4>
-                        {!book.available && <Button color="secondary" className="btn-primary" onClick={toggleIn}>Check-in</Button>}
-                        {book.available && <Button color="primary" className="btn-primary" onClick={toggleOut}>Check-out</Button>}
+                        {!book.available && <Button color="secondary" className="bookListBtn" onClick={toggleIn}>Check-in</Button>}
+                        {book.available && <Button color="primary" className="bookListBtn" onClick={toggleOut}>Check-out</Button>}
                     </Col>
-                
-            </Row>
            {outModal && <CheckOutModal modal={outModal} toggle={toggleOut} book={book} setUpdate={setUpdate}/>}
            {inModal && <CheckInModal modal={inModal} toggle={toggleIn} book_id={book.id} setUpdate={setUpdate}/>}
             </Col>
