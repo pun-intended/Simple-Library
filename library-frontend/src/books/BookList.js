8a9c560e6eb836f2b8e1847c777170e93192ed3d
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-06-22 17:23:26 +0900

    Add Stage Selection

--- library-frontend/src/books/BookList.js
+++ library-frontend/src/books/BookList.js
@@ -5,7 +5,7 @@
 import React, { useEffect, useState, useContext } from "react"
 import BookCard from "./BookCard";
 import LibraryApi from "../api";
-import {Container, Col, Row} from 'reactstrap'
+import {Container, Selection, Option, Row, FormGroup, Input} from 'reactstrap'
 import StudentContext from "../StudentContext";
 import "./BookList.css"
 
@@ -14,6 +14,7 @@ const BookList = () => {
     const {setStudents} = useContext(StudentContext)
     const [books, setBooks] = useState([])
     const [update, setUpdate] = useState(false)
+    const [stageData, setStage] = useState(null)
     useEffect( () => {
         async function initializeList(){
             const updateBooks = await LibraryApi.getAllBooks()
@@ -25,14 +26,39 @@ const BookList = () => {
         initializeList()
     }, [update])
 
+    const handleChange = (evt) => {
+        let stage = evt.target.value;
+        if(!stage){ stage = null }
+        console.log(stage)
+        setStage(stage)
+    }
+
     return(
         <Container className="BookList">
             <h1>All Books</h1>
+                <FormGroup
+                    onChange={handleChange}>
+                    <Input type="select">
+                        <option value=""> Select Stage </option>
+                        <option value="1"> Stage 1 </option>
+                        <option value="2"> Stage 2 </option>
+                        <option value="3"> Stage 3 </option>
+                    </Input>
+                </FormGroup>
                 <Row className="align-items-stretch"> 
                     {books.map((book) => {
-                        return(
-                            <BookCard book={book} setUpdate={setUpdate} key={book.id}/> 
-                        )
+                        if(stageData === null){
+                            return(
+                                <BookCard book={book} setUpdate={setUpdate} key={book.id}/> 
+                            )   
+                        } else {
+                            if(book.stage == stageData){
+                                return(
+                                    <BookCard book={book} setUpdate={setUpdate} key={book.id}/> 
+                                )
+                            }
+                        }
+                    
                     })}
                 </Row>     
         </Container>
